ef78050145e33d2c5db8a083f273fe561763e106
Coles <macols@f8ffc2474896.ant.amazon.com>
2022-08-12 20:01:49 +1000

    mew

---
+++ next/next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    experimental: {
+      externalDir: true,
+    },
+    webpack: config => {
+      config.resolve.alias["react/jsx-runtime"] = require.resolve("react/jsx-runtime")
+      config.resolve.alias["react/jsx-dev-runtime"] = require.resolve("react/jsx-dev-runtime")
+      return config
+    },
+  }
---
+++ next/pages/index.js
@@ -0,0 +1,9 @@
+import HelloWorld from '../../next-components/components/hello-world'
+
+export default function Home() {
+  return (
+    <div className="app">
+      <HelloWorld />
+    </div>
+  )
+}
